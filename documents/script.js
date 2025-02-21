4f1eed3a3b1c9b2fb2f635275285355da72b0e89
Valen Li <117173876+li-valen@users.noreply.github.com>
2025-02-20 23:41:08 -0500

    documents page

--- documents/script.js
+++ documents/script.js
@@ -1,70 +1,74 @@
-const UnsplashImage = ({ url, key }) => /*#__PURE__*/
-React.createElement("div", { className: "image-item", key: key }, /*#__PURE__*/
-React.createElement("img", { src: url }));
-
-
-
-let Collage = () => {
-  const [images, setImages] = React.useState([]);
-  const [loaded, setIsLoaded] = React.useState(false);
-
-  React.useEffect(() => {
-    fetchImages();
-  }, []);
-
-  const fetchImages = (count = 10) => {
-    const apiRoot = "https://api.unsplash.com";
-    const accessKey =
-    "25a6ce8664b4aef4027c7ca877021522ee500372ea770251b3ece9096788fe53";
-
-    axios.
-    get(`${apiRoot}/photos/random?client_id=${accessKey}&count=${count}`).
-    then(res => {
-      setImages([...images, ...res.data]);
-      setIsLoaded(true);
-      //console.log(images);
-    }).
-    catch(err => {
-      // console.log(err);
-      if (err.response.status === 403) {
-        console.log('There have been too many many API requests recently. Try again later.');
-      }
-    });
-  };
-
-  // main component
-  return /*#__PURE__*/(
-    React.createElement("div", { className: "hero is-fullheight is-bold" }, /*#__PURE__*/
-    React.createElement("div", { className: "hero-body" }, /*#__PURE__*/
-    React.createElement("div", { className: "container" }, /*#__PURE__*/
-    React.createElement("div", { className: "header content" }, /*#__PURE__*/
-    React.createElement("h2", { className: "subtitle is-6" }, "Code Challenge #16"), /*#__PURE__*/
-    React.createElement("h1", { className: "title is-1" }, "Infinite Scroll Gallery")), /*#__PURE__*/
-
-    React.createElement(InfiniteScroll, {
-      dataLength: images,
-      next: () => fetchImages(5),
-      hasMore: true,
-      loader: /*#__PURE__*/
-      React.createElement("span", { class: "loader" }) }, /*#__PURE__*/
-
-
-    React.createElement("div", { className: "image-grid", style: { marginTop: "60px" } },
-    loaded ?
-    images.map((image, index) => /*#__PURE__*/
-    React.createElement(UnsplashImage, { url: image.urls.regular, key: index })) :
-
-    ""))))));
-
-
-
-
-
-
+const UnsplashImage = ({ url }) => {
+  return React.createElement("div", { className: "image-item" }, 
+    React.createElement("img", { src: url, alt: "Gallery item" })
+  );
 };
 
-// Render the component to the DOM element with ID of root
-ReactDOM.render( /*#__PURE__*/React.createElement(Collage, null), document.getElementById("root"));
+let Collage = () => {
+  const allImages = [
+    "/images/image1.jpg",
+    "/images/image2.jpg",
+    "/images/image3.jpg",
+    "/images/image4.jpg",
+    "/images/image5.jpg",
+    "/images/image6.jpg",
+    "/images/image7.jpg",
+    "/images/image8.jpg",
+    "/images/image9.jpg",
+    "/images/image10.jpg",
+    "/images/image11.jpg",
+    "/images/image12.jpg",
+    "/images/image13.jpg",
+    "/images/image14.jpg",
+    "/images/image15.jpg",
+    "/images/image16.jpg",
+    "/images/image17.jpg",
+    "/images/image18.jpg",
+    "/images/image19.jpg",
+    "/images/image20.jpg",
+    "/images/image21.jpg",
+    "/images/image22.jpg",
+    "/images/image23.jpg",
+    "/images/image24.jpg",
+    "/images/image25.jpg",
+  ];
 
+  const [images, setImages] = React.useState(allImages.slice(0, 3)); // Load first 3 images
+  const [hasMore, setHasMore] = React.useState(true);
 
-/** This Gallery is a solution to a Code Challenge #16 by https://scotch.io **/
+  const loadMoreImages = () => {
+    setTimeout(() => {
+      if (images.length >= allImages.length) {
+        setHasMore(false);
+        return;
+      }
+      const nextBatch = allImages.slice(images.length, images.length + 3);
+      setImages([...images, ...nextBatch]);
+    }, 1000);
+  };
+
+  return React.createElement(
+    "div", { className: "hero is-fullheight is-bold" }, 
+    React.createElement("div", { className: "hero-body" }, 
+      React.createElement("div", { className: "container" }, 
+        React.createElement("div", { className: "header content" }, 
+          React.createElement("h2", { className: "subtitle is-6" }, "Code Challenge #16"),
+          React.createElement("h1", { className: "title is-1" }, "Infinite Scroll Gallery")
+        ),
+        React.createElement(InfiniteScroll, {
+          dataLength: images.length,
+          next: loadMoreImages,
+          hasMore: hasMore,
+          loader: React.createElement("span", { className: "loader" }, "Loading...")
+        }, 
+          React.createElement("div", { className: "image-grid", style: { marginTop: "60px" } },
+            images.map((image, index) => React.createElement(UnsplashImage, { url: image, key: index }))
+          )
+        )
+      )
+    )
+  );
+};
+
+// Render component
+ReactDOM.render(React.createElement(Collage), document.getElementById("root"));
